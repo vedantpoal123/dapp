c2a7b82fa5a14ae9b990335429d130c8f7aaa75d
vedantpoal123 <161232675+vedantpoal123@users.noreply.github.com>
2024-02-29 22:55:48 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# dapp
+This is a basic dapp to transfer ether from one account to another account using nextjs and tailwind.
+
+## Getting Started
+
+First, install all dependencies:
+
+```bash
+npm install
+# or
+yarn install
+```
+Then, run the development server: 
+
+```bash
+npm run dev
+# or
+yarn dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+## Preview
+![Screenshot (150)](https://github.com/gks2022004/ETH-transfer-dApp/assets/94637265/14367764-10c1-4044-b40c-b97b04dd3b62)
+
+
+If you like the project, support by giving it a star.
---
+++ pages/index.js
@@ -0,0 +1,152 @@
+import Head from 'next/head'
+import { useEffect, useState } from 'react';
+import { ethers } from 'ethers'
+import Profile from '../components/Profile';
+import SendEth from '../components/SendEth';
+import Transactions from '../components/Transactions';
+
+export default function Home() {
+
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [defaultAccount, setDefaultAccount] = useState(null);
+  const [userBalance, setUserBalance] = useState(null);
+  const [connButtonText, setConnButtonText] = useState('Connect Wallet');
+
+  const [address, setAddress] = useState()
+  const [amount, setAmount] = useState()
+
+  const [txs, setTxs] = useState();
+
+  const handleAddress = (e) => {
+    setErrorMessage("")
+    setAddress(e.target.value)
+  }
+
+  const handleAmount = (e) => {
+    console.log(typeof e.target.value)
+    setAmount(e.target.value)
+  }
+
+  const connectWalletHandler = () => {
+    if (window.ethereum && window.ethereum.isMetaMask) {
+      console.log('MetaMask Here!');
+
+      window.ethereum.request({ method: 'eth_requestAccounts' })
+        .then(result => {
+          setDefaultAccount(result[0]);
+          getAccountBalance(result[0].toString());
+          setConnButtonText('Wallet Connected');
+        })
+        .catch(error => {
+          setErrorMessage(error.message);
+        });
+
+    } else {
+      console.log('Need to install MetaMask');
+      setErrorMessage('Please install MetaMask browser extension to interact');
+    }
+  }
+
+  const getAccountBalance = (account) => {
+    window.ethereum.request({ method: 'eth_getBalance', params: [account, 'latest'] })
+      .then(balance => {
+        setUserBalance(ethers.utils.formatEther(balance));
+      })
+      .catch(error => {
+        setErrorMessage(error.message);
+      });
+  };
+
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      if (window.ethereum) {
+        console.log('MetaMask Here!');
+        window.ethereum.on('accountsChanged', (newAcc) => {
+          setDefaultAccount(newAcc);
+          getAccountBalance(newAcc.toString());
+        });
+        window.ethereum.on('chainChanged', () => {
+          window.location.reload();
+        });
+      } else {
+        console.log('Need to install MetaMask');
+        setErrorMessage('Please install MetaMask browser extension to interact');
+      }
+    }
+  }, [])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    try {
+      ethers.utils.getAddress(address)
+      setErrorMessage("")
+
+      const tx = await signer.sendTransaction({
+        to: address,
+        value: ethers.utils.parseEther(amount)
+      })
+
+      window.ethereum.request({ method: 'eth_requestAccounts' })
+        .then(result => {
+          setDefaultAccount(result[0]);
+          getAccountBalance(result[0].toString());
+          setConnButtonText('Wallet Connected');
+        })
+        .catch(error => {
+          setErrorMessage(error.message);
+        });
+      setTxs([tx])
+
+    } catch (err) {
+      setErrorMessage(err.message)
+    }
+  }
+
+  return (
+    <div className="min-h-screen flex-col flex justify-center items-center">
+      <Head>
+        <title>First DAPP</title>
+        <meta name="description" content="Dapp made by Gaurav Kumar" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {
+        errorMessage && (
+          <div className="alert alert-error mt-4">
+            <div className="flex-1">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-6 h-6 mx-2 stroke-current">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"></path>
+              </svg>
+              <label>{errorMessage}</label>
+            </div>
+          </div>
+        )
+      }
+
+      <div className="flex justify-center items-center w-full container p-4">
+
+        <div className='w-full'>
+          <div className='flex justify-center flex-col lg:flex-row'>
+            <Profile defaultAccount={defaultAccount} userBalance={userBalance} errorMessage={errorMessage} connectWalletHandler={connectWalletHandler} connButtonText={connButtonText} />
+
+            <SendEth handleAddress={handleAddress} handleAmount={handleAmount} handleSubmit={handleSubmit} errorMessage={errorMessage} defaultAccount={defaultAccount} />
+          </div>
+
+          <div className='mt-8'>
+            <Transactions txs={txs} />
+          </div>
+
+        </div>
+
+      </div>
+
+     
+
+
+    </div>
+  )
+}
